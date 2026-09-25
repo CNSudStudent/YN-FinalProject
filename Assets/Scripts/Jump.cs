b27bb295ed8c3177d4d0dd9f2077ac6006cc8fcd
CNSudStudent <221208971+CNSudStudent@users.noreply.github.com>
2026-09-25 17:38:31 -0400

    Initial check-in

---
+++ Assets/Scripts/Jump.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jump : MonoBehaviour
+{
+    // strength of the push
+    [Header("Jump Stremgth")]
+    public float jumpStrength = 10f;
+
+    [Header("Jump in Midair")]
+    public bool alwaysJumping = false;
+
+    [Header("Extra Jump")]
+    public float extraJump;
+    private float extrajumptracker;
+
+    private bool canJump;
+
+    private Rigidbody2D rigidBody;
+
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+        extrajumptracker = extraJump;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (rigidBody.linearVelocity.y == 0)
+        {
+            canJump = true;
+            extraJump = extrajumptracker;
+        }
+        else
+        {
+            canJump = false;
+        }
+
+        if ((canJump) && Input.GetButton("Jump"))
+        {
+            // Apply an instantaneous upwards force
+            rigidBody.AddForce(Vector2.up * jumpStrength, ForceMode2D.Impulse);
+            //canJump = !checkGround;
+
+        }
+        if ((extraJump>=1) && (canJump=false) && Input.GetButton("Jump"))
+        {
+            // Apply an instantaneous upwards force
+            rigidBody.AddForce(Vector2.up * jumpStrength, ForceMode2D.Impulse);
+            //canJump = !checkGround;
+            extraJump -= 1;
+
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerJump : MonoBehaviour
+{
+    public float jumpStrength = 10f;
+
+    public float fallMultiplier = 2.5f;
+    public float jumpMultiplier = 2f;
+
+    public bool alwaysJumping = false;
+
+    private bool canJump;
+
+    private Rigidbody rigidBody;
+
+    void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Mathf.Abs(rigidBody.linearVelocity.y) < 0.05)
+        {
+            canJump = true;
+        }
+        else
+        {
+            canJump = false;
+        }
+
+        if (Input.GetButtonDown("Jump") && canJump)
+        {
+            GetComponent<Rigidbody>().linearVelocity += Vector3.up * jumpStrength;
+
+        }
+    }
+}
